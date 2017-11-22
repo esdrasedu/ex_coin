7e18bad276792c201519d8e24ff3333b02c96560
Esdras Eduardo <esdrasedu@gmail.com>
2017-11-22 09:48:46 -0200

    using input and output, eq bitcoin (more easy to validate transaction)

---
+++ lib/blockchain/output.ex
@@ -0,0 +1,36 @@
+defmodule ExCoin.Blockchain.Output do
+  @moduledoc """
+  Defines a basic transfer operation
+
+  ```
+  %Output{
+    id: "id of output",
+    tx_id: "id of transaction",
+    previous_id: "id of input",
+    senderKey: "public key of transaction issuer",
+    recipient: "address of the recipient",
+    amount: 100_000,
+    fee: 10 # fee for miners
+  }
+  ```
+  """
+
+  defstruct [:id, :tx_id, :previous_id, :senderKey, :recipient, :amount, :fee]
+
+  @type t :: %__MODULE__{
+    id: String.t,
+    tx_id: String.t,
+    previous_id: String.t,
+    senderKey: String.t,
+    recipient: String.t,
+    amount: Integer.t,
+    fee: Integer.t
+  }
+
+  alias ExCoin.Blockchain.Output
+
+  def to_json(%Output{} = output) do
+    Poison.encode(output)
+  end
+
+end
